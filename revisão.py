920ebcd897683989eeace37a06966e4c5a53ef16
Daniel Marcos Alexandre Fernandes <dmarcos59@gmail.com>
2025-07-16 21:37:40 -0300

    Add files via upload

---
+++ revisão.py
@@ -0,0 +1,10 @@
+# modulo
+print(12 %4 ==0)
+n = int(input('Digite um número'))
+a = 10
+if(n % 2 == 0):
+    #print(f'o {n} digitado é par')
+    print('O número => ',n,'digitado é par')
+else:
+    #print(f'o {n} digitado é ímpar')
+    print('O número => ',n,' digitado é ímpar')    
