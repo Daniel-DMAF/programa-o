f552c6b2174c30cd3d31a104319c03a4397958c5
Daniel Marcos Alexandre Fernandes <dmarcos59@gmail.com>
2025-08-06 21:38:42 -0300

    Add files via upload

---
+++ aula 7/ex1.py
@@ -0,0 +1,8 @@
+
+fruta = []
+
+for i in range(3):
+    x = input(f'Digite o nome da {i} fruta')
+    fruta.append(x)
+print(f'lista de frutas {fruta}')    
+    
---
+++ aula 7/ex3.py
@@ -0,0 +1,15 @@
+fruta = ['maçã', 'banana', 'laranja', 'pitaya','mamão']
+
+print(f'Todas as frutas ===>{fruta [4]}')
+tamanho = len(fruta)
+print(f'O tamanho da lista é {tamanho}')
+print('.........')
+fruta[1] = 'uva'
+print(f'Todas as frutas ===>{fruta}')
+fruta.insert(1,'abacaxi')
+print(f'lista atualizada ===>{fruta}')
+
+# removendo pelo indice
+print(fruta)
+fruta.pop(1)
+print(fruta)
